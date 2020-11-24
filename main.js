6a27f2eb7228a2cf541f9ad3fa3e3ba295ec4ebf
anitasengupta <anita.m.sengupta@gmail.com>
2020-11-24 17:45:51 +0100

    opacity changes on footnote open and close

--- main.js
+++ main.js
@@ -1,43 +1,19 @@
-//specialization elements
-let specializationOverlay = document.getElementById('specialization-overlay')
+// variables
+let uxDesignMainContent = document.getElementById('ux-design');
+let uxDesignFootnote = document.getElementById('ux-design-footnote');
+let name = document.getElementById('name');
 
-// specialization main content
-let uxDesignMainContent = document.getElementById('ux-design')
-let brandCreationMainContent = document.getElementById('brand-creation')
-let printGraphicDesignMainContent = document.getElementById('print-graphic-design')
-let digitalGraphicDesignDesignMainContent = document.getElementById('digital-graphic-design')
-let specializationSpan1 = document.getElementById('specialization-span-1')
-let specializationSpan2 = document.getElementById('specialization-span-2')
-let specializationSpan3 = document.getElementById('specialization-span-3')
-let specializationSpan4 = document.getElementById('specialization-span-4')
-
-//specialization footnotes
-let uxDesignFootnote = document.getElementById('ux-design-footnote')
-
-// add hover
-
-function displayUxDesignFootnote(){
+// functions
+function showUxDesignFootnote() {
   uxDesignFootnote.style.display = 'block';
-  specializationOverlay.style.display = 'block';
-  //opacity of selected: 100%
-  //opacity of rest of paragraph: 10%
-  uxDesignMainContent.style.opacity= '1';
-  specializationSpan1.style.opacity = '0.2';
-  specializationSpan2.style.opacity = '0.2';
-  specializationSpan3.style.opacity = '0.2';
-  specializationSpan4.style.opacity = '0.2';
-  brandCreationMainContent.style.opacity = '0.2';
-  printGraphicDesignMainContent.style.opacity = '0.2';
-  digitalGraphicDesignDesignMainContent.style.opacity = '0.2';
+  uxDesignMainContent.classList.add('active-footnote-link');
 }
 
-function clearUxDesignFootnote(){
+function hideUxDesignFootnote() {
   uxDesignFootnote.style.display = 'none';
-  specializationOverlay.style.display = 'none';
-  //opacity of selected: 85%
-  //opacity of rest of paragraph: 85%
-  specializationMainContent.style.opacity = '0.85';
+  uxDesignMainContent.classList.remove('active-footnote-link');
 }
 
-uxDesignMainContent.onclick = displayUxDesignFootnote;
-specializationOverlay.onclick = clearUxDesignFootnote;
+//event listeners
+uxDesignMainContent.onclick = showUxDesignFootnote;
+name.onclick = hideUxDesignFootnote;
