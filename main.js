017047347514e5547b5d256c4fec157ba228698d
anitasengupta <anita.m.sengupta@gmail.com>
2020-11-25 14:30:52 +0100

    refactored footnote close transition

--- main.js
+++ main.js
@@ -1,8 +1,5 @@
 // variables
 
-  // grid
-  // const specializationGrid = document.getElementById('specialization');
-
   // specialization main content
   const specializationNonLinkedContent1 = document.getElementById('specialization-non-linked-content-1');
   const specializationNonLinkedContent2 = document.getElementById('specialization-non-linked-content-2');
@@ -33,34 +30,7 @@ function showUxDesignFootnote() {
 }
 
 function hideUxDesignFootnote() {
-  uxDesignFootnote.style.transition = 'opacity .2s ease-out 0s';
-  uxDesignFootnote.classList.remove('active-footnote');
-  uxDesignMainContent.style.transition = 'opacity .2s ease-out 0s';
-  uxDesignMainContent.classList.remove('active-footnote-link');
-  specializationNonLinkedContent1.style.transition = 'opacity .2s ease-in 0s';
-  specializationNonLinkedContent1.classList.remove('inactive-main-content');
-  specializationNonLinkedContent2.style.transition = 'opacity .2s ease-in 0s';
-  specializationNonLinkedContent2.classList.remove('inactive-main-content');
-  specializationNonLinkedContent3.style.transition = 'opacity .2s ease-in 0s';
-  specializationNonLinkedContent3.classList.remove('inactive-main-content');
-  specializationNonLinkedContent4.style.transition = 'opacity .2s ease-in 0s';
-  specializationNonLinkedContent4.classList.remove('inactive-main-content');
-  brandCreationMainContent.style.transition = 'opacity .2s ease-in 0s';
-  brandCreationMainContent.classList.remove('inactive-main-content');
-  printGraphicDesignMainContent.style.transition = 'opacity .2s ease-in 0s';
-  printGraphicDesignMainContent.classList.remove('inactive-main-content');
-  digitalGraphicDesignMainContent.style.transition = 'opacity .2s ease-in 0s';
-  digitalGraphicDesignMainContent.classList.remove('inactive-main-content');
-}
-
-//event listeners
-uxDesignMainContent.onclick = showUxDesignFootnote;
-// specializationGrid.onclick = hideUxDesignFootnote;
-// name.onclick = hideUxDesignFootnote;
-
-//scratch
-window.addEventListener('mouseup', function(event){
-  if(event.target != /*name*/ uxDesignFootnote && event.target.parentNode != uxDesignFootnote) {
+  if(event.target != uxDesignFootnote && event.target.parentNode != uxDesignFootnote) {
     uxDesignFootnote.style.transition = 'opacity .2s ease-out 0s';
     uxDesignFootnote.classList.remove('active-footnote');
     uxDesignMainContent.style.transition = 'opacity .2s ease-out 0s';
@@ -80,4 +50,8 @@ window.addEventListener('mouseup', function(event){
     digitalGraphicDesignMainContent.style.transition = 'opacity .2s ease-in 0s';
     digitalGraphicDesignMainContent.classList.remove('inactive-main-content');
   }
-})
+}
+
+//event listeners
+uxDesignMainContent.onclick = showUxDesignFootnote;
+window.onmouseup = hideUxDesignFootnote;
