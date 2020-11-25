dd304e441d9efe5e544dfb0c9a2a47ffadc07fb9
anitasengupta <anita.m.sengupta@gmail.com>
2020-11-25 11:17:03 +0100

    fixes to footnote transition animation

--- main.js
+++ main.js
@@ -10,6 +10,7 @@
   const digitalGraphicDesignMainContent = document.getElementById('digital-graphic-design');
 
   // specialization footnotes
+  // const specializationOverlay = document.querySelector('specialization-overlay');
   const uxDesignFootnote = document.getElementById('ux-design-footnote');
 
   // other
@@ -18,6 +19,7 @@
 // functions
 function showUxDesignFootnote() {
   uxDesignFootnote.classList.add('active-footnote');
+  uxDesignMainContent.classList.add('active-footnote-link');
   specializationNonLinkedContent1.classList.add('inactive-main-content');
   specializationNonLinkedContent2.classList.add('inactive-main-content');
   specializationNonLinkedContent3.classList.add('inactive-main-content');
@@ -51,3 +53,29 @@ function hideUxDesignFootnote() {
 //event listeners
 uxDesignMainContent.onclick = showUxDesignFootnote;
 name.onclick = hideUxDesignFootnote;
+
+//scratch
+/* window.addEventListener('mouseup', function(event){
+  if (event.target != uxDesignFootnote && event.target.parentNode != uxDesignFootnote) {
+    uxDesignFootnote.style.transition = 'opacity 2s ease-out 0s';
+    uxDesignFootnote.classList.remove('active-footnote');
+    uxDesignMainContent.style.transition = 'opacity .2s ease-out 0s';
+    uxDesignMainContent.classList.remove('active-footnote-link');
+    specializationNonLinkedContent1.style.transition = 'opacity .2s ease-in 0s';
+    specializationNonLinkedContent1.classList.remove('inactive-main-content');
+    specializationNonLinkedContent2.style.transition = 'opacity .2s ease-in 0s';
+    specializationNonLinkedContent2.classList.remove('inactive-main-content');
+    specializationNonLinkedContent3.style.transition = 'opacity .2s ease-in 0s';
+    specializationNonLinkedContent3.classList.remove('inactive-main-content');
+    specializationNonLinkedContent4.style.transition = 'opacity .2s ease-in 0s';
+    specializationNonLinkedContent4.classList.remove('inactive-main-content');
+    brandCreationMainContent.style.transition = 'opacity .2s ease-in 0s';
+    brandCreationMainContent.classList.remove('inactive-main-content');
+    printGraphicDesignMainContent.style.transition = 'opacity .2s ease-in 0s';
+    printGraphicDesignMainContent.classList.remove('inactive-main-content');
+    digitalGraphicDesignMainContent.style.transition = 'opacity .2s ease-in 0s';
+    digitalGraphicDesignMainContent.classList.remove('inactive-main-content');
+  }
+}); */
+
+// && event.target.parentNode != specializationOverlay
