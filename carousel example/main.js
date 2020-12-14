e31ff8c7c4b941b654505a4750f5e41a845865ff
anitasengupta <anita.m.sengupta@gmail.com>
2020-12-14 20:30:12 +0100

    clickable carousel that changes active state based on scroll position

--- carousel example/main.js
+++ carousel example/main.js
@@ -1,47 +1,31 @@
 const indicators = document.querySelectorAll('.control li');
 const captions = document.querySelectorAll('.caption p');
 const sections = document.querySelectorAll('section');
-// const fromTop = window.scrollY;
-// index = 0;
 
-// reflect scroll position
-// if sections[i] is in viewport & sections[i-1] is not:
-// ... set index to i
-// ... remove selected class from any indicator that has it
-// ... add selected class to indicators[i]
-// ... remove selected-caption class from any caption that has it
-// ... add selected-caption class to captions[i]
+// click
+indicators.forEach((indicator, i) => {
+	indicator.addEventListener('click', () => {
+	    sections[i].scrollIntoView();
+	 });
+});
 
-//Active navigation on scroll
-var isInViewport = function (elem) {
-	var distance = elem.getBoundingClientRect();
+// changing styles on scroll
+const isInViewport = function (elem) {
+	const distance = elem.getBoundingClientRect();
 	return (
 		distance.top >= distance.height * -1 &&
 		distance.bottom <= (window.innerHeight || document.documentElement.clientHeight)
 	);
 };
 
-var specialization = document.querySelector('#specialization');
-
-window.addEventListener('scroll', function (event) {
-	if (isInViewport(specialization)) {
-		console.log('In viewport!');
-	} else {
-    console.log('Nope...');
-  }
-}, false);
-
-
-// hover
-
-// click
-indicators.forEach((indicator, i) => {
-	indicator.addEventListener('click', () => {
+window.addEventListener('scroll', event => {
+  sections.forEach((section, i) => { 
+	if (isInViewport(sections[i])) {
 		document.querySelector('.control .selected').classList.remove('selected');
-	    indicator.classList.add('selected');
-	    // index = i;
+	    indicators[i].classList.add('selected');
 	    document.querySelector('.selected-caption').classList.remove('selected-caption');
 	    captions[i].classList.add('selected-caption');
-	    sections[i].scrollIntoView();
-	 });
+    } else {
+    }
+  });
 });
