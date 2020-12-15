37fe4e0e0217dbe32e42a0b441b5825fcdea49bd
anitasengupta <anita.m.sengupta@gmail.com>
2020-12-15 21:43:30 +0100

    refactoring changing carousel styles on scroll

--- main.js
+++ main.js
@@ -336,46 +336,43 @@ const captions = document.querySelectorAll('.caption p');
 const sections = document.querySelectorAll('section');
 
 // changing styles on scroll
-const isInViewport = function (elem) {
+function isInViewport(elem) {
   const distance = elem.getBoundingClientRect();
   return (
     distance.top >= distance.height * -1 &&
     distance.bottom <= (window.innerHeight || document.documentElement.clientHeight)
   );
-};
+}
 
-window.addEventListener('scroll', event => {
+function resetActiveCarouselStyles(i) {
+  // const indicators = document.querySelectorAll('.control li');
+  // const captions = document.querySelectorAll('.caption p');
+  document.querySelector('.control .selected').classList.remove('selected');
+  indicators[i].classList.add('selected');
+  document.querySelector('.selected-caption').classList.remove('selected-caption');
+  captions[i].classList.add('selected-caption');
+}
+
+function reflectScrollPosition() {
+  // const sections = document.querySelectorAll('section');
   sections.forEach((section, i) => { 
   if (isInViewport(sections[i])) {
-    document.querySelector('.control .selected').classList.remove('selected');
-      indicators[i].classList.add('selected');
-      document.querySelector('.selected-caption').classList.remove('selected-caption');
-      captions[i].classList.add('selected-caption');
-    } else {
+      resetActiveCarouselStyles(i);
     }
   });
-});
+}
+
+window.onscroll = reflectScrollPosition;
 
 // indicator behavior on hover and click
 indicators.forEach((indicator, i) => {
   indicator.addEventListener('click', () => {
-      sections[i].scrollIntoView();
+    sections[i].scrollIntoView();
    });
   indicator.addEventListener('mouseover', () => {
-      document.querySelector('.control .selected').classList.remove('selected');
-      indicators[i].classList.add('selected');
-      document.querySelector('.selected-caption').classList.remove('selected-caption');
-      captions[i].classList.add('selected-caption');
+    resetActiveCarouselStyles(i);
    });
   indicator.addEventListener('mouseout', () => {
-      sections.forEach((section, i) => { 
-      if (isInViewport(sections[i])) {
-        document.querySelector('.control .selected').classList.remove('selected');
-          indicators[i].classList.add('selected');
-          document.querySelector('.selected-caption').classList.remove('selected-caption');
-          captions[i].classList.add('selected-caption');
-        } else {
-        }
-      });
+    reflectScrollPosition();
    });
 });
